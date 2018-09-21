f6e0d4839e20a913db5cee8af57e931ad7af85a7
Jeffrey <JeffreyPadgett1@gmail.com>
2018-09-20 20:19:33 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//Listen for submit click button
+document.getElementById('entire-form').addEventListener('submit',calculatePrice);
+
+//Calculate Price
+function calculatePrice(e){
+    console.log('Calculating...');
+
+    e.preventDefault();
+}
