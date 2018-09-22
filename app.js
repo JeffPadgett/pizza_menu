5361d81d033adbc3a039fe48272315e40cbfc7e4
Jeffrey <JeffreyPadgett1@gmail.com>
2018-09-21 21:05:01 -0700

    commit with included rough draft

--- app.js
+++ app.js
@@ -1,53 +1,47 @@
-//Declare variables for the form
-const pizzaSelection = document.querySelector('.pizza-selection');
+// Declare and devine UI variables
+const form = document.querySelector('#entire-form');
+const selectedPizza = document.querySelector('input[name="pizzachoice"]:checked');
+const selectedCheese = document.querySelector('input[name="optcheese"]:checked');
+const selectedSauce = document.querySelector('input[name="optsauce"]:checked');
+const selectedCrust = document.querySelector('input[name="optcrust"]:checked');
+const selectedMeats = document.querySelectorAll('input[name="meatTopping"]:checked');
+const selectedVeggies = document.querySelectorAll('input[name="veggieTopping"]:checked');
+const makePizzaBtn = document.querySelector('#make-pizza');
+const cancenBtn = document.querySelector('.my-button');
+
+//Runs all the event listeners
+function loadEventListeners() {
+
+    makePizzaBtn.addEventListener('submit', getPizzaSizeValue);
 
 
-//Listen for submit click button
-document.getElementById('entire-form').addEventListener('submit',calculatePrice);
-
-//Calculate Price
-function calculatePrice(e){
-    var total=0.0;
-    var toppingMultiplyer = 1;
-    //selects the actual pizza size from the radio buttons. 
-    var pizzaSize = document.querySelector('input[name="optradio"]:checked').value;
-    //a variable that holds an array and selects ALL the items checked
-    var meatToppings = document.querySelectorAll('input[name="meatTopping"]:checked');
-    // a veriable that holds an array of all the items checked in veggies
-    var veggieToppings = document.querySelectorAll('input[name="veggieTopping"]:checked');
-    //a If loop that will ensure that the price is correct. 
-    if (meatToppings.length > 0) {
-        total += meatToppings.length - 1;
-    
-    }
-
-    //An if looop that will ensure the price is correct for the veggie side box. 
-    if (veggieToppings.length > 0) {
-        total += veggieToppings.length - 1;
-    }
+}
 
 
 
-    //If statement is designed to keep the user from not selecting a pizza size
-    if(pizzaSize.value == '') {
-        alert('Please select a pizza size');
-    }
 
+//Gets the price of pizza and stores it in a variable
+function getPizzaSizeValue() {
 
     
-//brought the recept variable that will hold value of the actual recept.
-    var recieptToppings = "";
 
-//Calls get recept
-  getReciept();  
+    if (selectedPizza.value == "Personal"){
+        
+        pizzaCost = 6
+    } else {
+        alert('Please select a pizza')
+    }
 
 
-  e.preventDefault();
+    console.log(pizzaCost);
 }
-//A function that sets the empty input value to the value of the selected pizza size.
-function getReciept(){
-    document.getElementById("pizza-price").value = document.querySelector('input[name="optradio"]:checked').value;
-}
+
+
+
+
+
+
+
 
 
 
