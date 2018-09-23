94f13336d5b408409cc300c0960cc8f84b937340
Jeffrey <JeffreyPadgett1@gmail.com>
2018-09-23 16:43:31 -0700

    flow chart and updated reciept

--- app.js
+++ app.js
@@ -1,47 +1,198 @@
 // Declare and devine UI variables
+console.log("loading js file")
 const form = document.querySelector('#entire-form');
-const selectedPizza = document.querySelector('input[name="pizzachoice"]:checked');
-const selectedCheese = document.querySelector('input[name="optcheese"]:checked');
-const selectedSauce = document.querySelector('input[name="optsauce"]:checked');
-const selectedCrust = document.querySelector('input[name="optcrust"]:checked');
-const selectedMeats = document.querySelectorAll('input[name="meatTopping"]:checked');
-const selectedVeggies = document.querySelectorAll('input[name="veggieTopping"]:checked');
-const makePizzaBtn = document.querySelector('#make-pizza');
+let selectedPizza = document.getElementsByClassName('pizza-choice');
+let selectedCheese = document.querySelector('input[name="optcheese"]:checked');
+let selectedSauce = document.querySelector('input[name="optsauce"]:checked');
+let selectedCrust = document.querySelector('input[name="optcrust"]:checked');
+let selectedMeats = document.querySelectorAll('input[name="meatTopping"]:checked');
+let selectedVeggies = document.querySelectorAll('input[name="veggieTopping"]:checked');
+const makePizzaBtn = document.getElementById('make-pizza');
 const cancenBtn = document.querySelector('.my-button');
+let selectedPizzaFinal;
+let selectedCheeseFinal;
+let selectedCrustFinal;
+let selectedSauceFinal;
 
-//Runs all the event listeners
-function loadEventListeners() {
 
-    makePizzaBtn.addEventListener('submit', getPizzaSizeValue);
-
+makePizzaBtn.onclick = function(){
+    getSelectedPizza();
+    getSelectedCheese();
+    getSelectedCrust();
+    getSelectedSauce();
 
 }
+function getSelectedPizza(){
+    var selectedPizza = document.getElementsByClassName('pizza-choice');
 
-
-
-
-//Gets the price of pizza and stores it in a variable
-function getPizzaSizeValue() {
-
+    for (var i = 0, length = selectedPizza.length; i < length; i++)
+    {
+     if (selectedPizza[i].checked)
+     {
+         selectedPizzaFinal = selectedPizza[i].value;    
+         
+         
+         break;
+        }
+    }
     
-
-    if (selectedPizza.value == "Personal"){
+    var sizePrice;
+    
+    switch(selectedPizzaFinal){
+        default: sizePrice = 1.00;
+        break;
         
-        pizzaCost = 6
-    } else {
-        alert('Please select a pizza')
+        case "Personal":
+        sizePrice = 6.00;
+        break;
+        
+        case "Medium":
+        sizePrice = 10.00;
+        break;
+        
+        case "Large":
+        sizePrice = 14.00;
+        break;
+        
+        case "Extra Large":
+        sizePrice = 16.00;
+        break;
+    }
+    
+    document.getElementById('size-reciept').value = `${selectedPizzaFinal} price: $${sizePrice}`;
+}
+
+function getSelectedCheese(){
+
+    var selectedCheese = document.getElementsByClassName('cheese-choice');
+
+    for (var i = 0, length = selectedCheese.length; i < length; i++)
+    {
+        if (selectedCheese[i].checked)
+        {
+            document.getElementById('cheese-reciept').value = selectedCheese[i].value;
+
+            selectedCheeseFinal = selectedCheese[i].value;
+
+            break;
+        }
     }
 
+    var cheesePrice;
+    
+    switch(selectedCheeseFinal){
+        default: cheesePrice = 1.00;
+        break;
+        
+        case "Regular Cheese":
+        cheesePrice = 0.00;
+        break;
+        
+        case "No Cheese":
+        cheesePrice = 0.00;   //may be an issue when it comes to creating the CreatePrice Function
+        break;
+        
+        case "Extra Cheese":
+        cheesePrice = 3.00;
+        break;
+        
+    }
+    
+    document.getElementById('cheese-reciept').value = `${selectedCheeseFinal} price: $${cheesePrice}`;
 
-    console.log(pizzaCost);
 }
 
+function getSelectedCrust(){
+
+    var selectedCrust = document.getElementsByClassName('crust-choice');
+
+    for (var i = 0, length = selectedCrust.length; i < length; i++)
+    {
+        if (selectedCrust[i].checked)
+        {
+            document.getElementById('crust-reciept').value = selectedCrust[i].value;
+
+            selectedCrustFinal = selectedCrust[i].value;
+
+            break;
+        }
+    }
+
+    var crustPrice;
+    
+    switch(selectedCrustFinal){
+        default: crustPrice = 1.00;
+        break;
+        
+        case "Plain Crust":
+        crustPrice = 0.00 ;
+        break;
+        
+        case "Garlic Butter Crust":
+        crustPrice = 3.00;   
+        break;
+        
+        case "Cheese Stuffed Crust":
+        crustPrice = 3.00;
+        break;
+
+        case "Spicy Crust":
+        crustPrice = 3.00;
+
+        case "House Special Crust":
+        crustPrice = 3.00;
+        
+    }
+
+    document.getElementById('crust-reciept').value = `${selectedCrustFinal} price: $${crustPrice}`;
+
+
+}
+
+function getSelectedSauce(){
+
+    var selectedSauce = document.getElementsByClassName('sauce-choice');
+
+    for (var i = 0, length = selectedSauce.length; i < length; i++)
+    {
+        if (selectedSauce[i].checked)
+        {
+            document.getElementById('sauce-reciept').value = selectedSauce[i].value;
+
+            selectedSauceFinal = selectedSauce[i].value;
+
+            break;
+        }
+    }
+
+    var crustPrice;
+    
+    switch(selectedCrustFinal){
+        default: crustPrice = 1.00;
+        break;
+        
+        case "Plain Crust":
+        crustPrice = 0.00 ;
+        break;
+        
+        case "Garlic Butter Crust":
+        crustPrice = 3.00;   
+        break;
+        
+        case "Cheese Stuffed Crust":
+        crustPrice = 3.00;
+        break;
+
+        case "Spicy Crust":
+        crustPrice = 3.00;
+
+        case "House Special Crust":
+        crustPrice = 3.00;
+        
+    }
+
+    document.getElementById('crust-reciept').value = `${selectedCrustFinal} price: $${crustPrice}`;
 
 
 
-
-
-
-
-
-
+}
